9d8ef60b264f3f99ea2c3db01ef20f7fab6d8d1b
Mike D <michael.mcdaniel210@gmail.com>
2018-02-25 17:50:51 -0600

    fixed routing issue, api/friends now shows

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res){
-        res.json(friendsdata);
+        res.json(friendsData);
     });
     app.post("/api/friends", function(req, res){
         friendsData = [];
