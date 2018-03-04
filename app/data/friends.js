237bf23e7b16cd9cc6dcb234311f5cbd789360a4
Mike D <michael.mcdaniel210@gmail.com>
2018-03-04 15:56:28 -0600

    working on for loop, added permanent 'users' to friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,24 @@
-friendsArray = [];
+friendsArray = [
+    {
+        lovebird: "John",
+        photo: "https://goo.gl/images/ghTiZJ",
+        scores: ['1', '3', '5', '4', '1', '2']
+    },
+    {
+        lovebird: "Paul",
+        photo: "https://goo.gl/images/2uv2y2",
+        scores: ['1', '3', '5', '4', '1', '2'] 
+    },
+    {
+        lovebird: "George",
+        photo: "https://goo.gl/images/vVp978",
+        scores: ['1', '3', '5', '4', '1', '2']
+    },
+    {
+        lovebird: "Ringo",
+        photo: "https://goo.gl/images/FgaKr4",
+        scores: ['1', '3', '5', '4', '1', '2']
+    },   
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,21 +5,24 @@ module.exports = function(app) {
         res.json(friendsData);
     });
     app.post("/api/friends", function(req, res){
-        var potentialMatch = req.body;
-        console.log(potentialMatch)
-        friendsData.push(potentialMatch);
+        var currentUser = req.body;
+        console.log(currentUser)
+        friendsData.push(currentUser);
         res.json(true);
-        // var potentialScores = potentialMatch.scores;
-        // console.log(potentialScores);  no idea why this is undefined,
-        //but i'm getting a weird thing with the way my scores are returning in the object
-
+        var currentScores = currentUser.scores;
     });
+
+    for(var i=0; i < friendsData.length; i ++){
+        // for(var j=0; j < currentScores.length; j ++){
+
+        // }
+    }
 }
-// loop through the friendsData arrays
+
 // for(var i=0; i < friendsData.length; i ++){
 // create a variable to store the compared scores    
 //     var compare = 0;
-// loop through scores of each     
+   
 //     for(var j=0; j < potentialScores.length; i++){
 //         compare += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(potentialScorej])) (i do think i need parse int b/c my values are integers)
 //     }
