f38c5015f3e23c7cd810c7ecf142af596e22612a
Mike D <michael.mcdaniel210@gmail.com>
2018-03-05 18:14:12 -0600

    bunches of changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ friendsArray = [
     {
         lovebird: "John",
         photo: "https://goo.gl/images/ghTiZJ",
-        scores: ['1', '3', '5', '4', '1', '2']
+        scores: ['1', '1', '1', '1', '1', '1']
     },
     {
         lovebird: "Paul",
         photo: "https://goo.gl/images/2uv2y2",
-        scores: ['1', '3', '5', '4', '1', '2'] 
+        scores: ['4', '4', '4', '4', '4', '4'] 
     },
     {
         lovebird: "George",
         photo: "https://goo.gl/images/vVp978",
-        scores: ['1', '3', '5', '4', '1', '2']
+        scores: ['3', '3', '3', '3', '3', '3']
     },
     {
         lovebird: "Ringo",
         photo: "https://goo.gl/images/FgaKr4",
-        scores: ['1', '3', '5', '4', '1', '2']
+        scores: ['2', '2', '2', '2', '2', '2']
     },   
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,28 +7,34 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res){
         var currentUser = req.body;
         console.log(currentUser)
-        friendsData.push(currentUser);
-        res.json(true);
         var currentScores = currentUser.scores;
+        var matchName = "";
+        var matchImg = "";
+        // var compareArray = [];
+        // var bestMatch = 0;
+        var compareDiff = 1000;
+        for (var i = 0; i < friendsData.length; i++) {
+            var compareScore = 0
+            for (var j = 0; j < currentScores.length; j++) {
+                compareScore += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(currentScores[j]));
+
+
+                }
+            if (compareScore < compareDiff) {
+                compareDiff = compareScore;
+                matchName = friendsData[i].lovebird;
+                matchImg = friendsData[i].photo;
+                console.log(matchImg);
+                console.log(matchName);
+            }
+
+        }
+        friendsData.push(currentUser);// gonna want to put this at the end, otherwise i'm going to be comparing current user scores with themselves.
+        res.json({status: 'OK', matchName: matchName, matchImg: matchImg });
     });
 
-    for(var i=0; i < friendsData.length; i ++){
-        // for(var j=0; j < currentScores.length; j ++){
-
-        // }
-    }
+    
 }
 
-// for(var i=0; i < friendsData.length; i ++){
-// create a variable to store the compared scores    
-//     var compare = 0;
-   
-//     for(var j=0; j < potentialScores.length; i++){
-//         compare += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(potentialScorej])) (i do think i need parse int b/c my values are integers)
-//     }
 
-// }
-
-// here's where i'm lost, do i need another array to push all of the compared scores to?
-// then i pick the lowest of that array using Math.min() to return the results to the modal?
 
